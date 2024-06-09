b3792d9eec79ded95c303f1319860fd226cc9a67
Amandeep * <amandeep.1@globallogic.com>
2024-06-09 16:30:41 +0530

    hero carousel done

--- src/components/homepage/heroScreen/HeroScreen.tsx
+++ src/components/homepage/heroScreen/HeroScreen.tsx
@@ -1,22 +1,71 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "./heroscreen.css";
 
 const HeroScreen = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    // pauseOnHover: true,
+  };
+
   return (
-    <div className="heroscreen flex justify-center items-center h-screen">
-      <div className="text-center" style={{ backdropFilter: "blur(0px)" }}>
-        <p className="upperPara text-3xl">Discover the</p>
-        <p
-          className="lowerPara text-7xl mb-5"
-          style={{
-            color: "white",
-            textShadow: "2px 2px black",
-            fontWeight: "600",
-          }}
-        >
-          Most Engaging Places
-        </p>
-      </div>
+    <div className="heroscreen-container h-screen">
+      <Slider {...settings}>
+        <div className="heroscreen-slide1 pt-[15rem]">
+          <div className="text-center" style={{ backdropFilter: "blur(0px)" }}>
+            <p className="upperPara text-3xl">Discover the hidden Gems</p>
+            <p
+              className="lowerPara text-5xl text-7xl mb-5"
+              style={{
+                color: "white",
+                textShadow: "2px 2px black",
+                fontWeight: "600",
+              }}
+            >
+              Most Engaging Places
+            </p>
+          </div>
+        </div>
+        <div className="heroscreen-slide2 pt-[15rem]">
+          <div className="text-center" style={{ backdropFilter: "blur(0px)" }}>
+            <p className="upperPara text-3xl">Explore the World</p>
+            <p
+              className="lowerPara lg:text-7xl text-5xl mb-5"
+              style={{
+                color: "white",
+                textShadow: "2px 2px black",
+                fontWeight: "600",
+              }}
+            >
+              Hidden Gems Located In World
+            </p>
+          </div>
+        </div>
+        <div className="heroscreen-slide3 pt-[15rem]">
+          <div className="text-center" style={{ backdropFilter: "blur(0px)" }}>
+            <p className="upperPara text-3xl">Explore the World</p>
+            <p
+              className="lowerPara text-5xl text-7xl mb-5"
+              style={{
+                color: "white",
+                textShadow: "2px 2px black",
+                fontWeight: "600",
+              }}
+            >
+              Hidden Gems Located In World
+            </p>
+          </div>
+        </div>
+        {/* Add more slides as needed */}
+      </Slider>
     </div>
   );
 };
